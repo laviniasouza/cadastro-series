f5e56a0c3a8f42fafb4ac5736a9ba9750c34b562
laviniasouza <laviniasouza14-@hotmail.com>
2021-07-23 22:09:51 -0300

    Primeiro Commit

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using cadastro_series.Interfaces;
+
+namespace cadastro_series
+{
+    public class SerieRepositorio : IRepositorio<Series>
+    {
+        private List<Series> listaSerie = new List<Series>();
+		public void Atualiza(int id, Series objeto)
+		{
+			listaSerie[id] = objeto;
+		}
+
+		public void Exclui(int id)
+		{
+			listaSerie[id].Exclui();
+		}
+
+		public void Insere(Series objeto)
+		{
+			listaSerie.Add(objeto);
+		}
+
+		public List<Series> Lista()
+		{
+			return listaSerie;
+		}
+
+		public int ProximoId()
+		{
+			return listaSerie.Count;
+		}
+
+		public Series RetornaPorId(int id)
+		{
+			return listaSerie[id];
+		}
+    }
+}
